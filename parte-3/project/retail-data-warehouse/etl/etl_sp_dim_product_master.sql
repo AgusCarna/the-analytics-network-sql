ed5ec9d68605f37a93f1af295b4da19ce6a4dcda
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-06 18:58:28 +0100

    Create etl_sp_dim_product_master.sql

---
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_dim_product_master.sql
@@ -0,0 +1,25 @@
+create or replace procedure etl.sp_dim_product_master()
+language plpgsql as $$
+DECLARE
+  usuario varchar(10) := current_user ;
+BEGIN
+  usuario := current_user; 
+  with cte as (
+  select *,
+	    CASE 
+        WHEN lower(name) LIKE '%samsung%' THEN 'Samsung'
+        WHEN lower(name) LIKE '%philips%' THEN 'Phillips'
+        WHEN lower(name) LIKE 'levi%' THEN 'Levis'
+        WHEN lower(name) LIKE 'jbl%' THEN 'JBL'
+        WHEN lower(name) LIKE '%motorola%' THEN 'Motorola'
+        WHEN lower(name) LIKE 'tommy%' THEN 'TH'
+        ELSE 'Unknown' end as brand
+	  from stg.product_master
+  )
+insert into dim.product_master(product_code, name, category, subcategory, subsubcategory, material, color, origen, ean, is_active, has_bluetooth, size, brand)
+select * from cte
+  on conflict (product_code) do update
+  set product_code = excluded.product_code;
+  call etl.log(current_date, 'product_master','usuario'); -- SP dentro del SP product_master para dejar log
+END;
+$$;
