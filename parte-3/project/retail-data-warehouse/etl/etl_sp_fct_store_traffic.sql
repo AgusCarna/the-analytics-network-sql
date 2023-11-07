6b33b1309780b34273d11e822082aa1bb0b2bd11
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-08 00:54:58 +0100

    Create etl_sp_fct_store_traffic.sql

---
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_store_traffic.sql
@@ -0,0 +1,16 @@
+create or replace procedure etl.sp_fct_store_traffic()
+language plpgsql as $$
+DECLARE
+  usuario varchar(10) := current_user ;
+BEGIN
+  usuario := current_user; 
+  with cte as (
+    select store_id, cast (date as date) as date, traffic
+	  from stg.super_store_count
+    where date > '2023-11-01' --> u otra fecha donde empiece me interese empezar a considerar
+  )
+insert into fct.store_traffic(store_id, date, traffic)
+select * from cte
+  call etl.log(current_date, 'store_traffic','usuario'); -- SP dentro del SP store_traffic para dejar log
+END;
+$$;
