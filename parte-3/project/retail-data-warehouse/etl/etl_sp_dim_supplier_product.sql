8b3c9e63c39b750c3509388346a02391b703f53e
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-07 23:53:26 +0100

    Create etl_sp_dim_supplier_product.sql

---
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_dim_supplier_product.sql
@@ -0,0 +1,17 @@
+create or replace procedure etl.sp_dim_supplier_product()
+language plpgsql as $$
+DECLARE
+  usuario varchar(10) := current_user ;
+BEGIN
+  usuario := current_user; 
+  with cte as (
+  select *
+	from stg.suppliers
+  )
+insert into dim.suppliers(product_id, name, is_primary)
+select * from cte where is_primary is true
+ -- on conflict (product_code) do update
+ -- set product_code = excluded.product_code;
+  call etl.log(current_date, 'supplier_product','usuario'); -- SP dentro del SP supplier_product para dejar log
+END;
+$$;
