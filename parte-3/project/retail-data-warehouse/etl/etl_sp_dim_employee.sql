1044ac08a102d21edd4368775f126d1448470fd9
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-08 00:42:08 +0100

    Create etl_sp_dim_employee.sql

---
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_dim_employee.sql
@@ -0,0 +1,24 @@
+create or replace procedure etl.sp_dim_employee()
+language plpgsql as $$
+DECLARE
+  usuario varchar(10) := current_user ;
+BEGIN
+  usuario := current_user; 
+  with cte as (
+      select id, name, surname, start_date, end_date,
+        	case
+          		when end_date is null then current_date-start_date
+          		else end_date-start_date
+          		end as duration,
+          phone, country, province, store_id, position,
+        	case
+          		when end_date is null then true
+          		else false
+          		end as is_active
+	    from stg.employees
+  )
+insert into dim.employee(id, name, surname, start_date, end_date, duration, phone, country, province, store_id, position, active)
+select * from cte
+  call etl.log(current_date, 'employee','usuario'); -- SP dentro del SP employee para dejar log
+END;
+$$;
