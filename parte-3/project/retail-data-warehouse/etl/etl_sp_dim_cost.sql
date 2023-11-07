821ce3edc4510f5f9f479eee662252382f003efa
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-07 23:34:19 +0100

    Update etl_sp_dim_cost.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_dim_cost.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_dim_cost.sql
@@ -1,1 +1,17 @@
-Prueba
+create or replace procedure etl.sp_dim_cost()
+language plpgsql as $$
+DECLARE
+  usuario varchar(10) := current_user ;
+BEGIN
+  usuario := current_user; 
+  with cte as (
+  select *
+	from stg.cost
+  )
+insert into dim.cost(product_id, cost_usd)
+select * from cte
+  --on conflict (product_code) do update
+  --set product_code = excluded.product_code;
+  call etl.log(current_date, 'cost','usuario'); -- SP dentro del SP cost para dejar log
+END;
+$$;
