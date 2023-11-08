9e89ce4a2f218a8d7359d1f533747f3348f7f962
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-08 01:15:24 +0100

    Create etl_sp_fct_inventory.sql

---
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_inventory.sql
@@ -0,0 +1,16 @@
+create or replace procedure etl.sp_fct_inventory()
+language plpgsql as $$
+DECLARE
+  usuario varchar(10) := current_user ;
+BEGIN
+  usuario := current_user; 
+  with cte as (
+    select *
+	  from stg.inventory
+    where date > '2023-11-01' --> u otra fecha donde empiece me interese empezar a considerar
+  )
+insert into fct.inventory (date, store_id, item_id, initial, final)
+select * from cte
+  call etl.log(current_date, 'inventory','usuario'); -- SP dentro del SP inventory para dejar log
+END;
+$$;
