374a30e036493e1fb535a2a68d10c8ddce10b892
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-08 01:18:26 +0100

    Create etl_sp_fct_return_movements.sql

---
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_return_movements.sql
@@ -0,0 +1,16 @@
+create or replace procedure etl.sp_fct_return_movements()
+language plpgsql as $$
+DECLARE
+  usuario varchar(10) := current_user ;
+BEGIN
+  usuario := current_user; 
+  with cte as (
+    select *
+	  from stg.return_movements
+    where date > '2023-11-01' --> u otra fecha donde empiece me interese empezar a considerar
+  )
+insert into fct.return_movements (order_id, return_id, item, quantity, movement_id, from_location, to_location, received_by, date)
+select * from cte
+  call etl.log(current_date, 'return_movements','usuario'); -- SP dentro del SP return_movements para dejar log
+END;
+$$;
