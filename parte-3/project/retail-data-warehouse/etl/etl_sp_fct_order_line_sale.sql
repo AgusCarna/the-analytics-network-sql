de3bfd8a9606255355f6b0be8dbc959ebe8f59bb
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-08 01:12:01 +0100

    Create etl_sp_fct_order_line_sale.sql

---
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_order_line_sale.sql
@@ -0,0 +1,17 @@
+create or replace procedure etl.sp_fct_order_line_sale()
+language plpgsql as $$
+DECLARE
+  usuario varchar(10) := current_user ;
+BEGIN
+  usuario := current_user; 
+  with cte as (
+    select *
+	  from stg.order_line_sale
+    where date > '2023-11-01' --> u otra fecha donde empiece me interese empezar a considerar
+  )
+insert into fct.order_line_sale (order_id, product_id, store_id, date, quantity, sale, 
+            promotion, tax, credit, currency, pos, is_walkout, line_key)
+select * from cte
+  call etl.log(current_date, 'order_line_sale','usuario'); -- SP dentro del SP order_line_sale para dejar log
+END;
+$$;
