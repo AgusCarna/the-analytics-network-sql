dbc49b171fcad78bfbe5c7a885c27d28c9ff07b8
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-08 14:22:20 +0100

    Create analytics_conversion.sql

---
+++ parte-3/project/retail-data-warehouse/analytics/analytics_conversion.sql
@@ -0,0 +1,15 @@
+--Creación función de conversión
+create function analytics.conversion(currency character varying(3), valor numeric, rate_peso numeric, rate_eur numeric, rate_uru numeric) 
+returns numeric as
+$$
+select
+		case
+			when currency = 'ARS'
+			then valor/rate_peso
+			when currency = 'EUR'
+			then valor/rate_eur
+			when currency = 'URU'
+			then valor/rate_uru
+			else valor
+			end as valor_usd
+$$ language sql;
