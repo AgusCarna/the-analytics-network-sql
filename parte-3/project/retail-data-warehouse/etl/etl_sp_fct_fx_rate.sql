914cd98880aca285c43f5185f3beee4426155c0d
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-08 01:00:20 +0100

    Create etl_sp_fct_fx_rate.sql

---
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_fx_rate.sql
@@ -0,0 +1,16 @@
+create or replace procedure etl.sp_fct_rate()
+language plpgsql as $$
+DECLARE
+  usuario varchar(10) := current_user ;
+BEGIN
+  usuario := current_user; 
+  with cte as (
+    select *
+	  from stg.monthly_average_fx_rate
+    where date > '2023-11-01' --> u otra fecha donde empiece me interese empezar a considerar
+  )
+insert into fct.rate (month, fx_rate_usd_peso, fx_rate_usd_eur, fx_rate_usd_uru)
+select * from cte
+  call etl.log(current_date, 'rate','usuario'); -- SP dentro del SP rate para dejar log
+END;
+$$;
