26fb690813196e226b0b512726a6c45d72b223bf
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-10 01:58:54 +0100

    Create analytics_return.sql

---
+++ parte-3/project/retail-data-warehouse/analytics/analytics_return.sql
@@ -0,0 +1,37 @@
+create or replace procedure analytics.sp_return()
+language plpgsql as $$
+truncate analytics.return
+DECLARE
+  usuario varchar(10) := current_user ;
+BEGIN
+  usuario := current_user; 
+  with cte as (
+
+select 
+
+--atributos devoluciones
+	rm.order_id, rm.return_id, rm.quantity, rm.date,
+--atributos producto
+	pm.product_id, pm.name as name_prod, pm.category, pm.subcategory, pm.subsubcategory, pm.material, pm.color, pm.origin, pm.ean, pm.is_active, pm.has_bluetooth, pm.size, pm.brand,
+--atributos tienda
+	sm.store_id, sm.country, sm.province, sm.city, sm.address, sm.name as name_store, sm.type, sm.start_date, sm.latitude, sm.longitude,
+--valor de venta producto retornado	
+((analytics.conversion(ols.currency, ols.sale, fx.fx_rate_usd_peso, fx.fx_rate_usd_eur, fx.fx_rate_usd_uru))/ols.quantity) as cost_dev_usd
+
+from fct.order_line_sale as ols
+left join dim.product_master as pm
+	on pm.product_id = ols.product_id
+inner join fct.return_movements as rm
+	on rm.order_id = ols.order_id
+	and rm.product_id = ols.product_id
+	and rm.movement_id = 2
+left join dim.store_master as sm
+	on sm.store_id = ols.store_id
+left join fct.fx_rate as fx
+	on (cast((date_trunc('month',ols.date)) as date)) = fx.month
+
+  )
+select * into analytics.return from cte 
+  call etl.log(current_date, 'return','usuario'); -- SP dentro del SP return para dejar log
+END;
+$$;
