dab9faa768c0ec03fadca901a043cd1daa05db55
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-10 02:37:57 +0100

    Create analytics_inventory.sql

---
+++ parte-3/project/retail-data-warehouse/analytics/analytics_inventory.sql
@@ -0,0 +1,33 @@
+create or replace procedure analytics.sp_inventory()
+language plpgsql as $$
+truncate analytics.inventory
+DECLARE
+  usuario varchar(10) := current_user ;
+BEGIN
+  usuario := current_user; 
+  with cte as (
+
+select 
+
+--atributos inventarios
+	i.date, ((i.initial+i.final)/2) as inv_prom,
+--atributos producto
+	pm.product_id, pm.name as name_prod, pm.category, pm.subcategory, pm.subsubcategory, pm.material, pm.color, pm.origin, pm.ean, pm.is_active, pm.has_bluetooth, pm.size, pm.brand,
+--atributos tienda
+	sm.store_id, sm.country, sm.province, sm.city, sm.address, sm.name as name_store, sm.type, sm.start_date, sm.latitude, sm.longitude,
+--costo inventario
+	c.cost_usd
+
+from fct.inventory as i
+left join dim.product_master as pm
+	on pm.product_id = i.item_id
+left join dim.store_master as sm
+	on sm.store_id = i.store_id
+left join dim.cost as c
+	on c.product_id = i.item_id
+
+  )
+select * into analytics.inventory from cte 
+  call etl.log(current_date, 'inventory','usuario'); -- SP dentro del SP inventory para dejar log
+END;
+$$;
