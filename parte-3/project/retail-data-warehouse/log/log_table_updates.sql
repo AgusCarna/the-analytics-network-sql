20392fae0746e54d446c7a271fe89b1207316232
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-10 02:51:44 +0100

    Update log_table_updates.sql

--- parte-3/project/retail-data-warehouse/log/log_table_updates.sql
+++ parte-3/project/retail-data-warehouse/log/log_table_updates.sql
@@ -9,3 +9,13 @@ CREATE TABLE IF NOT EXISTS log.table_updates
                           , stored_procedure    VARCHAR(30)
                           , username            VARCHAR(30)
 );
+
+--------------------------------------------------------------
+
+--/*CREO TABLA DE LOGS*/
+CREATE TABLE etl.log
+                 (
+                              fecha date
+                            , tabla          VARCHAR(255)
+                            , usuario        VARCHAR(255)
+					 )
