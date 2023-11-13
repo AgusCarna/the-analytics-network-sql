94f832b04c7692a7bd68c2f7677ecd78e6b78e40
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-13 18:30:17 +0100

    Update etl_sp_fct_order_line_sale.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_order_line_sale.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_order_line_sale.sql
@@ -11,7 +11,7 @@ BEGIN
   )
 insert into fct.order_line_sale (order_id, product_id, store_id, date, quantity, sale, 
             promotion, tax, credit, currency, pos, is_walkout, line_key)
-select * from cte
+select * from cte;
   call etl.log('fct.order_line_sale',current_date, 'sp_fct_order_line_sale','usuario'); -- SP dentro del SP order_line_sale para dejar log
 END;
 $$;
