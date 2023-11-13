01bcc26702c8dce20bd760ce6ca3aeaa5d79ab93
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-13 18:17:29 +0100

    Update etl_sp_fct_return_movements.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_return_movements.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_return_movements.sql
@@ -11,6 +11,6 @@ BEGIN
   )
 insert into fct.return_movements (order_id, return_id, item, quantity, movement_id, from_location, to_location, received_by, date)
 select * from cte
-  call etl.log(current_date, 'return_movements','usuario'); -- SP dentro del SP return_movements para dejar log
+  call etl.log('fct.return_movements',current_date, 'sp_fct_return_movements','usuario'); -- SP dentro del SP return_movements para dejar log
 END;
 $$;
