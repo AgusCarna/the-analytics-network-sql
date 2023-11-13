9d50afbb20d0a3a695c0939991a2e3bc5ff6667a
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-13 18:31:23 +0100

    Update etl_sp_fct_shrinkage.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_shrinkage.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_shrinkage.sql
@@ -10,7 +10,7 @@ BEGIN
  --   where date > '2023-11-01' --> u otra fecha donde empiece me interese empezar a considerar
   )
 insert into fct.shrinkage (year, store_id, item_id, quantity)
-select * from cte
+select * from cte;
   call etl.log('fct.shrinkage',current_date, 'sp_fct_shrinkage','usuario'); -- SP dentro del SP shrinkage para dejar log
 END;
 $$;
