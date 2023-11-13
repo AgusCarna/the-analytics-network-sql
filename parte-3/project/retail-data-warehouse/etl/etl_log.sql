1849148851086ac2ea247c8368de66c89468ffe7
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-14 00:29:49 +0100

    Update etl_log.sql

--- parte-3/project/retail-data-warehouse/etl/etl_log.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_log.sql
@@ -1,11 +1,3 @@
---/*CREO TABLA DE LOGS*/
-CREATE TABLE etl.log
-                 (
-                              fecha date
-                            , tabla          VARCHAR(255)
-                            , usuario       VARCHAR(255)
-					 )
-
 --/Creo stored procedure/
 create or replace procedure etl.log(parametro_tabla VARCHAR(30), parametro_fecha date, parametro_sp varchar(10), parametro_usuario varchar(10))
 language sql as $$
