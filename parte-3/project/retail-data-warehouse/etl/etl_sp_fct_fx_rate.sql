2ec821d77de559b9cb06587bc86f7396d19b4405
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-13 18:28:48 +0100

    Update etl_sp_fct_fx_rate.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_fx_rate.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_fx_rate.sql
@@ -10,7 +10,7 @@ BEGIN
     where date > '2023-11-01' --> u otra fecha donde empiece me interese empezar a considerar
   )
 insert into fct.fx_rate (month, fx_rate_usd_peso, fx_rate_usd_eur, fx_rate_usd_uru)
-select * from cte
+select * from cte;
   call etl.log('fct.fx_rate',current_date, 'sp_fct_rate','usuario'); -- SP dentro del SP rate para dejar log
 END;
 $$;
