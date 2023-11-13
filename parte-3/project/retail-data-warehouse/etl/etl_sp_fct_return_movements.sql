44ce1e44974805170e2369322b7ae2c930d75515
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-13 18:30:49 +0100

    Update etl_sp_fct_return_movements.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_return_movements.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_return_movements.sql
@@ -10,7 +10,7 @@ BEGIN
     where date > '2023-11-01' --> u otra fecha donde empiece me interese empezar a considerar
   )
 insert into fct.return_movements (order_id, return_id, item, quantity, movement_id, from_location, to_location, received_by, date)
-select * from cte
+select * from cte;
   call etl.log('fct.return_movements',current_date, 'sp_fct_return_movements','usuario'); -- SP dentro del SP return_movements para dejar log
 END;
 $$;
