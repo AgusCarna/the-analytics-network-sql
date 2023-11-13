ffcbe3c8901c5e576f75a7864c88aa96549dac25
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-13 18:29:11 +0100

    Update etl_sp_fct_inventory.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_inventory.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_inventory.sql
@@ -10,7 +10,7 @@ BEGIN
     where date > '2023-11-01' --> u otra fecha donde empiece me interese empezar a considerar
   )
 insert into fct.inventory (date, store_id, item_id, initial, final)
-select * from cte
+select * from cte;
   call etl.log('fct.inventory',current_date, 'sp_fct_inventory','usuario'); -- SP dentro del SP inventory para dejar log
 END;
 $$;
