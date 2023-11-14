af477012df1165967accb79df5a507490c00783f
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-14 01:56:02 +0100

    Update analytics_inventory.sql

--- parte-3/project/retail-data-warehouse/analytics/analytics_inventory.sql
+++ parte-3/project/retail-data-warehouse/analytics/analytics_inventory.sql
@@ -1,8 +1,8 @@
 create or replace procedure analytics.sp_inventory()
 language plpgsql as $$
-DECLARE
+declare
   usuario varchar(10) := current_user ;
-BEGIN
+begin
   usuario := current_user;
   truncate analytics.inventory;
   with cte as (
@@ -10,13 +10,13 @@ BEGIN
 select 
 
 --atributos inventarios
-	i.date, ((i.initial+i.final)/2) as inv_prom,
+	i.date, ((i.initial+i.final)/2) as inv_prom
 --atributos producto
-	pm.product_id, pm.name as name_prod, pm.category, pm.subcategory, pm.subsubcategory, pm.material, pm.color, pm.origin, pm.ean, pm.is_active, pm.has_bluetooth, pm.size, pm.brand,
+	, pm.product_id, pm.name as name_prod, pm.category, pm.subcategory, pm.subsubcategory, pm.material, pm.color, pm.origin, pm.ean, pm.is_active, pm.has_bluetooth, pm.size, pm.brand
 --atributos tienda
-	sm.store_id, sm.country, sm.province, sm.city, sm.address, sm.name as name_store, sm.type, sm.start_date, sm.latitude, sm.longitude,
+	, sm.store_id, sm.country, sm.province, sm.city, sm.address, sm.name as name_store, sm.type, sm.start_date, sm.latitude, sm.longitude
 --costo inventario
-	c.cost_usd
+	, c.cost_usd
 
 from fct.inventory as i
 left join dim.product_master as pm
@@ -30,11 +30,13 @@ left join dim.cost as c
 insert into analytics.inventory select * from cte; 
   call etl.log('inventory',current_date, 'sp_inventory','usuario'); -- SP dentro del SP inventory para dejar log
 	IF 
-	(select product_id, date, store_id, count(1)
-	from cte
-	group by product_id, date, store_id
-	having count(1) > 1) 
-	is NOT NULL THEN RAISE EXCEPTION 'ERROR';
-	END IF;
+		(select product_id, date, store_id, count(1)
+		from cte
+		group by product_id, date, store_id
+		having count(1) > 1) 
+		is NOT NULL THEN RAISE EXCEPTION 'ERROR'
+		;
+	END IF
+	;
 END;
 $$;
