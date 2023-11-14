b84d3d701d9b7b0cc6954aaa0267c01a427de9f0
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-14 01:37:26 +0100

    Update etl_sp_dim_date.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_dim_date.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_dim_date.sql
@@ -1,3 +1,34 @@
+--Modifico la stg.date para que pase a tener un rango de 5 años mediante un truncate & insert
+
+truncate stg.date 
+
+insert into stg.date 
+select
+	date_in as date,
+	extract(month from date_in) as month,
+	extract(year from date_in) as year,
+	to_char(date_in, 'day') as weekday,
+	case
+		WHEN extract(dow FROM date_in) IN (0, 6) THEN 'true'
+		else false
+		end as is_weekend,
+	to_char(date_in, 'month') as month_label,
+	case
+		when extract (month from date_in) in (1) then concat ('FY',cast(((extract(year from date_in))-1) as text))
+		else concat ('FY',cast((extract(year from date_in)) as text))
+		end as fiscal_year_label,
+	case
+		when extract (month from date_in) in (2,3,4) then 'Q1'
+		when extract (month from date_in) in (5,6,7) then 'Q2'
+		when extract (month from date_in) in (8,9,10) then 'Q3'
+		when extract (month from date_in) in (11,12,1) then 'Q4'
+		end as fiscal_quarter_label,
+	(date_trunc('year', date_in) - interval '1 year') + (date_in - DATE_TRUNC('year', date_in)) AS date_ly	
+from
+	(select cast ('2022-01-01' as date) + (n || 'day')::interval as date_in
+	from generate_series (0,1825) n );
+
+--Creamos el stored procedure llamando a la tabla de stg actualizada
 create or replace procedure etl.sp_dim_date()
 language plpgsql as $$
 DECLARE
