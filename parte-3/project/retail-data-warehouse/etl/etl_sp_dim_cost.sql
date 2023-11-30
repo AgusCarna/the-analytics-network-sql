3f8f362f0737b5aa8023f38576f3fb571c980fdd
AgusCarna <144354164+AgusCarna@users.noreply.github.com>
2023-11-29 22:19:38 -0300

    Update etl_sp_dim_cost.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_dim_cost.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_dim_cost.sql
@@ -9,6 +9,8 @@ BEGIN
 	from stg.cost
   )
 insert into dim.cost(product_id, cost_usd)
+	  on conflict (product_id) do update
+ 	 set cost_usd = excluded.cost_usd;
 select * from cte;
   call etl.log('dim.cost',current_date, 'sp_dim_cost','usuario'); -- SP dentro del SP cost para dejar log
 END;
